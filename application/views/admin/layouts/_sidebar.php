9af4d95c9f74385a3b3cf0152f91125b51d2b756
Muhammad Kuswari <muh.kuswari10@gmail.com>
2021-05-30 23:18:00 +0800

    add bank managment link to sidebar

--- application/views/admin/layouts/_sidebar.php
+++ application/views/admin/layouts/_sidebar.php
@@ -31,6 +31,7 @@
 				</ul>
 			</li>
 			<li><a class="nav-link" href="<?= base_url("kelola-orderan") ?>"><i class="fas fa-credit-card"></i> <span>Kelola Transaksi</span></a></li>
+			<li><a class="nav-link" href="<?= base_url("admin/bank") ?>"><i class="fas fa-credit-card"></i> <span>Kelola Rekening</span></a></li>
 			<li><a class="nav-link" href="<?= base_url("laporan") ?>"><i class="fas fa-file"></i> <span>Laporan</span></a></li>
 			<li><a class="nav-link" href="<?= base_url("admin/profile") ?>"><i class="fas fa-user-cog"></i> <span>Profile Saya</span></a></li>
 			<li><a class="nav-link text-danger" href="<?= base_url("admin/logout") ?>"><i class="fas fa-sign-out-alt"></i> <span>Keluar</span></a></li>
